00e38e483a63353c00598cb956e45af55bc8508c
pat-s <patrick.schratz@gmail.com>
2026-04-21 19:45:07 +0200

    feat: make S3 endpoint and region configurable via env vars
    Read CRANLIKE_S3_ENDPOINT and CRANLIKE_S3_REGION in .onLoad, falling back to the current Backblaze defaults.

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,8 @@
   s3fs::s3_file_system(
     aws_access_key_id = Sys.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
-    endpoint = "https://s3.eu-central-003.backblazeb2.com",
-    region_name = "eu-central-003",
+    endpoint = Sys.getenv("CRANLIKE_S3_ENDPOINT", "https://s3.eu-central-003.backblazeb2.com"),
+    region_name = Sys.getenv("CRANLIKE_S3_REGION", "eu-central-003"),
   )
-} 
+}
 
